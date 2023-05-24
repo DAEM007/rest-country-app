6c992a291080712430092fc1b336723b5f392a9f
Emmanuel <dammylola299@gmail.com>
2023-05-24 18:58:52 +0100

    updated SelectBar

--- src/components/SelectBar.tsx
+++ src/components/SelectBar.tsx
@@ -1,22 +1,26 @@
 import { Select } from "@chakra-ui/react";
+import "./SelectBar.css";
 
 const SelectBar = () => {
     return (
         <Select placeholder='Filter by Region'
             background="white"
-            width="240px"
-            size="lg"
+            width="200px"
+            // size="lg"
             border="none"
             p="5px"
             mt={{ base: '40px', md: '0px' }}
             borderRadius="5px"
             boxShadow="0px 1px 10px hsl(0, 0%, 90%)"
+            fontWeight="semibold"
         >
-            <option value='option1'>Africa</option>
-            <option value='option2'>America</option>
-            <option value='option3'>Asia</option>
-            <option value='option3'>Europe</option>
-            <option value='option3'>Oceania</option>
+            
+            <option className="option" value='option1'>Africa</option>
+            <option className="option" value='option2'>America</option>
+            <option className="option" value='option3'>Asia</option>
+            <option className="option" value='option3'>Europe</option>
+            <option className="option" value='option3'>Oceania</option>
+            
         </Select>
     );
 }
