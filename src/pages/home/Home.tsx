c21f37b7967092cb76bbc8d9561b7473e241d89e
Emmanuel <dammylola299@gmail.com>
2023-05-24 15:52:00 +0100

    started integrating API

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,31 @@
 import { 
     Box,
+    Card,
     Flex,
+    Image,
+    SimpleGrid,
 } from "@chakra-ui/react";
 import SearchBar from "../../components/SearchBar";
 import SelectBar from "../../components/SelectBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+    const [countries, setCountries] = useState<any[]>([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios('https://restcountries.com/v3.1/all')
+            .then((response) => { 
+                console.log(response.data);
+                setCountries(response.data);
+            })
+            .catch((error) => {
+                console.log(`sorry! couldn't fetch data from API`);
+                setError(error);
+            })
+    }, [])
+
     return (
         <Box p="40px" pl="50px" pr="70px" boxSizing="border-box">
             <Flex 
@@ -16,6 +36,28 @@ const Home = () => {
                 <SearchBar />
                 <SelectBar />
             </Flex>
+
+            <SimpleGrid 
+                columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
+                spacing='40px'
+                m="40px"
+                ml="4px"
+                mr="5px"
+            >
+                { countries && countries.map((country, index) => (
+                    <Card
+                        key={index}
+                    >
+                        <Box>
+                            <Image 
+                                src={country.flags.png}
+                                alt={country.flags.alt}
+                            />
+                        </Box>
+                    </Card>
+                )) }
+            </SimpleGrid>
+
         </Box>
     );
 }
