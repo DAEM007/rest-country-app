0903fe11af889fb30875b3aa2d1c877edd86ae07
Emmanuel <dammylola299@gmail.com>
2023-05-25 10:36:54 +0100

    updated Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,25 +6,32 @@ import {
 } from "@chakra-ui/react";
 import SearchBar from "../../components/SearchBar";
 import SelectBar from "../../components/SelectBar";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import CardInner from "../../components/CardInner";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
     const [countries, setCountries] = useState<any[]>([]);
     const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
 
+    // fetch countries data
     useEffect(() => {
+        setIsPending(true);
+
         axios('https://restcountries.com/v3.1/all')
             .then((response) => { 
                 console.log(response.data);
                 setCountries(response.data);
+                setIsPending(false);
             })
             .catch((error) => {
                 console.log(`sorry! couldn't fetch data from API`);
                 setError(error);
+                setIsPending(false);
             })
-    }, [])
+
+    }, []);
 
     return (
         <Box p="40px" pl="50px" pr="70px" boxSizing="border-box">
@@ -44,7 +51,7 @@ const Home = () => {
                 ml="4px"
                 mr="5px"
             >
-                { countries && countries.map((country, index) => (
+                { countries && countries.map((country: any, index: any) => (
                     <Card
                         key={index}
                     >
@@ -52,9 +59,11 @@ const Home = () => {
                     </Card>
                 )) }
 
-                { error && <Box>{ error }</Box> }
-
             </SimpleGrid>
+
+            {isPending && <Box>Loading...</Box>}
+
+            { error && <Box>{ error }</Box> }
 
         </Box>
     );
